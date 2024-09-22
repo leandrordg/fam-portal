f0714d4e488a94c5bac44c66b38ce3bc871c4bc3
Leandro Rodrigues <lj251004@gmail.com>
2024-09-22 11:35:04 -0300

    added svix synchronization to project

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { ClerkLoading, SignedIn, UserButton } from "@clerk/nextjs";
 
 export function Header() {
   return (
     <header className="border-b shadow-sm">
       <nav className="flex items-center px-6 lg:px-10 py-4">
         <Link href="/">
-          <Image src="/fam-logo.png" alt="FAM Logo" width={100} height={100} />
+          <Image
+            src="/fam-logo.png"
+            alt="FAM Logo"
+            width={100}
+            height={100}
+            className="w-20 lg:w-24 object-contain"
+          />
         </Link>
 
         <div className="flex items-center ml-auto">
+          <ClerkLoading>
+            <div className="size-7 rounded-full bg-gray-100 animate-pulse" />
+          </ClerkLoading>
           <SignedIn>
             <UserButton />
           </SignedIn>
